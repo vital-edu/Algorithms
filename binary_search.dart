783981cca3d40c505c8f1c87ad8f87467d07b131
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-01-23 18:14:46 -0300

    Improve binary search

--- binary_search.dart
+++ binary_search.dart
@@ -1,24 +1,58 @@
+import 'utils/in.dart';
+import 'utils/std_in.dart';
+
+import 'utils/std_out.dart';
+
 class BinarySearch {
-  static int initRank(int key, List<int> list) {
-    return rank(key, list, 0, list.length - 1);
+  int rank(int key, List<int> list) {
+    return _rank(key, list, 0, list.length - 1);
   }
 
-  static int rank(int key, List<int> list, int li, int hb) {
+  int _rank(int key, List<int> list, int li, int hb) {
     if (li > hb) return null;
 
     int middle = li + (hb - li) ~/ 2;
     if (key < list[middle])
-      return rank(key, list, li, middle - 1);
+      return _rank(key, list, li, middle - 1);
     else if (key > list[middle])
-      return rank(key, list, middle + 1, hb);
+      return _rank(key, list, middle + 1, hb);
     else
       return middle;
   }
 }
 
-main(List<String> args) {
-  print(BinarySearch.initRank(1, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-  print(BinarySearch.initRank(4, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
-  print(BinarySearch.initRank(7, [1, 2, 3, 4, 5, 6, 7, 8]));
-  print(BinarySearch.initRank(10, [1, 2, 3, 4, 5, 6, 7, 8]));
+enum ReturnType {
+  notFound,
+  found,
+}
+main(List<String> args) async {
+  if (args == null || args.length < 1) return;
+
+  List<int> whitelist = await In.fromName(args[0]).readAllInts();
+  ReturnType type = ReturnType.notFound;
+
+  if (args.length > 1 && args[1] != null) {
+    if (args[1] == '+') {
+      type = ReturnType.notFound;
+    } else if (args[1] == '-') {
+      type = ReturnType.found;
+    }
+  }
+
+  whitelist.sort();
+
+  BinarySearch searcher = BinarySearch();
+
+  await for (int key in StdIn.readInt()) {
+    switch (type) {
+      case ReturnType.found:
+        if (searcher.rank(key, whitelist) != null)
+          StdOut.println('$key is in the list');
+        break;
+      case ReturnType.notFound:
+        if (searcher.rank(key, whitelist) == null)
+          StdOut.println('$key is not in the list');
+        break;
+    }
+  }
 }
