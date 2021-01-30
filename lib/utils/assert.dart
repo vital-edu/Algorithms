5ac72f34b76f04cda4f3397c0f670b4e51a72fac
Eduardo Vital Alencar Cunha <vital@hey.com>
2021-01-30 11:11:29 -0300

    Add new assert for iterables

--- lib/utils/assert.dart
+++ lib/utils/assert.dart
@@ -2,4 +2,19 @@ class Assert {
   static equal<T>(T a, T b) {
     assert(a == b, '$a != $b');
   }
+
+  static deepEqual(Iterable<dynamic> a, Iterable<dynamic> b) {
+    assert(
+        a.length == b.length, 'different lengths: ${a.length} != ${b.length}');
+
+    for (int i = 0; i < a.length; i++) {
+      try {
+        Iterable<dynamic> itA = Iterable.castFrom(a.elementAt(i));
+        Iterable<dynamic> itB = Iterable.castFrom(b.elementAt(i));
+        deepEqual(itA, itB);
+      } on TypeError {
+        equal(a.elementAt(i), b.elementAt(i));
+      }
+    }
+  }
 }
